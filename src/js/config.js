2be93ea4d3a00258b501095121b38809ede03d01
Akshai-Krishna-S <akshai404akz@gmail.com>
2021-08-28 01:18:36 +0530

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '5d1b35ef-98fb-4b66-bdfc-e580eb71efc2';
+export const MODAL_CLOSE_SEC = 2.5;
